c9d03f2c47eb6e5f84fd11e61adbf39e78a3bce3
Eli Schwartz <141594493+Eli-J-Schwartz@users.noreply.github.com>
2026-04-22 14:56:39 -0400

    Set Up Docker (#6)

---
+++ docker_halt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "This script must be run with sudo, like so: sudo $0" 
+  exit
+fi
+if ! command -v docker &> /dev/null;
+  then echo "Docker installation not found. Please install docker." 
+  exit 1
+fi
+docker container stop rpi_games_docker_container
