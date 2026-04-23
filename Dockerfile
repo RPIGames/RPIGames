97e610c0e757df2177470b2afa807f24d5a32da0
Eli Schwartz <141594493+Eli-J-Schwartz@users.noreply.github.com>
2026-04-23 12:43:56 -0400

    Add NGINX with forwarding to FastAPI (#9)
    * configure docker to use nginx, forward /api/ to fastapi
    
    * allow forwarding to fastapi

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.14
-WORKDIR /app
-COPY ./src/backend/requirements.txt /app/requirements.txt
-RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
-COPY ./src /app/src
-CMD ["fastapi", "run", "src/backend/main.py", "--port", "80"]
---
+++ docker_logs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]
+  then echo "This script must be run with sudo, like so: sudo $0" 
+  exit 1
+fi
+if ! command -v docker &> /dev/null;
+  then echo "Docker installation not found. Please install docker." 
+  exit 1
+fi
+sudo docker container logs rpi_games_docker_container
