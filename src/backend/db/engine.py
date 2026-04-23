6a65756a14daab648a73bd594fc14492974d685a
Daniel Soria <80708654+Dan-megabyte@users.noreply.github.com>
2026-04-23 16:04:31 -0400

    exclusively catch KeyError for DATABASE_PATH

--- src/backend/db/engine.py
+++ src/backend/db/engine.py
@@ -6,7 +6,7 @@ from .models import Lobby, User
 
 try:
     sqlite_file_name = os.environ["DATABASE_PATH"]
-except:
+except KeyError:
     sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
