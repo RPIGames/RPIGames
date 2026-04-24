00272e007647cdacab7b33480dbfaf17fc616426
Eli Schwartz <141594493+Eli-J-Schwartz@users.noreply.github.com>
2026-04-24 09:34:07 -0400

    Add Database to Docker (#13)
    * Make Persistent DB in Docker (#11)
    
    * configure docker to use nginx, forward /api/ to fastapi
    
    * allow forwarding to fastapi
    
    * added volume with persistent db to docker
    
    * reorganize import statements in engine.py
    
    * reorder import statements to put standard imports over third-library imports
    
    * exclusively catch KeyError for DATABASE_PATH
    
    * fix api docs access
    
    ---------
    
    Co-authored-by: Daniel Soria <80708654+Dan-megabyte@users.noreply.github.com>

--- deploy/start_server.sh
+++ deploy/start_server.sh
@@ -1,2 +1,2 @@
 nginx
-fastapi run src/backend/main.py --port 9000 --forwarded-allow-ips="*"
+fastapi run src/backend/main.py --port 9000 --forwarded-allow-ips="*" --root-path /api
--- docker_reset_db.sh
+++ docker_reset_db.sh
@@ -7,8 +7,6 @@ if ! command -v docker &> /dev/null;
   then echo "Docker installation not found. Please install docker." 
   exit 1
 fi
-docker build -t rpi_games_docker_image . -f deploy/Dockerfile
 docker container stop rpi_games_docker_container
 docker container rm rpi_games_docker_container
-docker volume create rpi_games_docker_volume
-docker run -d --name rpi_games_docker_container  --mount type=volume,src=rpi_games_docker_volume,dst=/app/db -p 80:80 rpi_games_docker_image
+docker volume rm rpi_games_docker_volume
