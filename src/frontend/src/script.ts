9f0b9c84f858ea5e387cab6efd74771a7d92bec6
soriad <soriad@rpi.edu>
2026-05-31 20:57:50 -0700

    add notification boxes to the frontend

--- src/frontend/src/script.ts
+++ src/frontend/src/script.ts
@@ -8,7 +8,7 @@ type PublicUserInfo = {
     name: string;
     leader: boolean;
     lobby_id: string | null;
-}
+};
 
 async function get_new_user_id() {
     let response = await fetch(
@@ -29,13 +29,58 @@ async function get_new_user_id() {
     }
 };
 
+enum MessageType {
+    Info = "info",
+    Success = "success",
+    Warning = "warning",
+    Error = "error",
+};
+
+export async function send_UI_notification(message: string, type: MessageType = MessageType.Info) {
+    console.log(`Sending ${type} notification with message:`, message);
+
+    const notification_div = document.createElement('div');
+    notification_div.classList.add('alert');
+    notification_div.classList.add(type.toString());
+
+    const notification_close_button = document.createElement('span');
+    notification_close_button.innerHTML = '&times;';
+    notification_close_button.classList.add('closebtn');
+
+    notification_close_button.onclick = (_) => {
+        notification_div.style.opacity = "0";
+        setTimeout(() => {
+            notification_div.remove();
+        }, 600);
+    };
+
+    const notification_message = document.createTextNode(message);
+
+    notification_div.appendChild(notification_close_button);
+    notification_div.appendChild(notification_message);
+
+    document.getElementById('notification-box')?.appendChild(notification_div);
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ping_button = document.getElementById("ping-button");
+    if (ping_button != null) {
+        ping_button.onclick = () => {send_UI_notification("pong!");};
+    }
+});
+
 async function get_user_info(user_id: string) {
     let response = await fetch(`/api/v1/user/info?user_id=${user_id}`);
     if (response.ok) {
         let info_response: PublicUserInfo = await response.json();
         return info_response;
+    } else if (response.status == 502) {
+        // this means there was a gateway error, which is probably because the backend is down
+        await send_UI_notification("The backend server seems to be down. Try checking back in in a couple hours, or contact the hostmaster.", MessageType.Error)
+        return null;
     } else {
-        console.log(`Couldn't get user info for user ${user_id}.`, await response.json());
+        console.log(`Couldn't get user info for user ${user_id}, since response code was ${response.status}.`);
         return null;
     }
 };
