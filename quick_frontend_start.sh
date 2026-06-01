4a8a048726415dcb8a50d6589e4ee3dc18e7ac13
soriad <soriad@rpi.edu>
2026-05-31 19:19:04 -0700

    improve quick start/stop scripts

--- quick_frontend_start.sh
+++ quick_frontend_start.sh
@@ -5,4 +5,10 @@ if ! command -v nginx &> /dev/null;
   exit 1
 fi
 
+if [[ -f /tmp/nginx.pid ]]; then
+  echo "Nginx has already been started. Stop nginx or delete /tmp/nginx.pid if nginx stopped incorrectly."
+  exit 1
+fi
+
 nginx -p . -c deploy/dev/nodocker/nginx_config_quick.conf 2> /dev/null
+printf "Frontend should be started at http://localhost:5000/\n"
--- quick_frontend_stop.sh
+++ quick_frontend_stop.sh
@@ -5,4 +5,9 @@ if ! command -v nginx &> /dev/null;
   exit 1
 fi
 
+if [[ ! -f /tmp/nginx.pid ]]; then
+  echo "Nginx hasn't been started yet. Cannot stop nginx."
+  exit 1
+fi
+
 nginx -p . -c deploy/dev/nodocker/nginx_config_quick.conf -s stop
