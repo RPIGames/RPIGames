51fdae5f365fa3f8f06434ed72d54258e0a76634
soriad <soriad@rpi.edu>
2026-09-01 18:54:45 -0400

    add comments

--- src/backend/db/default/random_username.py
+++ src/backend/db/default/random_username.py
@@ -1,9 +1,15 @@
+"""
+Simple default generator for a random username.
+"""
+
 import random
 
 from .lists import adjective_list, noun_list
 
 def random_username() -> str:
+    """Returns a random username consisting of an adjective and a noun."""
     return random.choice(adjective_list).capitalize() + " " + random.choice(noun_list).capitalize()
 
+# Also doubles as a tool for generating random usernames for testing and such
 if __name__ == "__main__":
     print(f"A random username: {random_username()}")
