40143503930b128a882e5edd85b9fb8e498748ae
soriad <soriad@rpi.edu>
2026-09-24 09:21:02 -0400

    add local linting when compiling typescript

--- compile_typescript.sh
+++ compile_typescript.sh
@@ -5,4 +5,10 @@ if ! command -v tsc &> /dev/null;
   exit 1
 fi
 
+if command -v biome &> /dev/null; then
+    biome check
+else
+    echo "Biome has not been found on PATH, so local linting will not run. You can instead rely on the remote github linter."
+fi
+
 tsc -p $(dirname "$0")"/src/frontend/tsconfig.json"
